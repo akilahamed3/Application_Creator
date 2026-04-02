33a5e1b4e56d107aa44c6724f897078c0036076a
Akil Ahamed <akilahamed3@outlook.com>
2026-04-02 20:30:09 +0530

    planner node completion

--- agent/graph.py
+++ agent/graph.py
@@ -1,22 +1,28 @@
 from dotenv import load_dotenv
-
 load_dotenv()
-
 from langchain_groq import ChatGroq
+from agent.prompts import *
+from agent.states import *
+from langgraph.constants import END
+from langgraph.graph import StateGraph
 
-from prompts import *
-
-from states import *
-
-from pydantic import BaseModel, Field
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
+def planner_agent(state: dict) -> dict:
+    user_prompt = state["user_prompt"]
+    resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    return {"plan": resp}
+
+
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
+graph.set_entry_point("planner")
+
+agent = graph.compile()
+
 user_prompt = "create a simple calculator web application"
 
-prompt = planner_prompt(user_prompt)
+result = agent.invoke({"user_prompt": user_prompt})
 
-
-resp = llm.with_structured_output(Plan).invoke(prompt)
-
-print(resp)
+print(result)
